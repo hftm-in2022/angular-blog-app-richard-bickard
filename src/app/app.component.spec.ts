70cc4015e6fabbe26287a45c7a9e71131480d95f
Richard Bickard <Richard.Bickard@hftm.ch>
2024-10-27 21:50:06 +0100

    fix problems

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, angular-blog-app-richard-bickard',
+      'angular-blog-app-richard-bickard',
     );
   });
 });
