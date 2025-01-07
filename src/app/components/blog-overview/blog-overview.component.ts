1166dc3ea6721ced8a63609e3d3a1862a7ae8540
Richard Bickard <Richard.Bickard@hftm.ch>
2025-01-07 23:52:37 +0100

    fix: lint-check

--- src/app/components/blog-overview/blog-overview.component.ts
+++ src/app/components/blog-overview/blog-overview.component.ts
@@ -1,37 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BlogEntry, BlogEntrySchema } from '../../types/blogEntry';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { BlogApiResponse, BlogApiResponseSchema } from '../../types/blogApiResponse';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BlogEntry, BlogEntrySchema } from "../../types/blogEntry";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import {
+  BlogApiResponse,
+  BlogApiResponseSchema,
+} from "../../types/blogApiResponse";
+import { CommonModule } from "@angular/common";
 
 const ValidationSchema = BlogApiResponseSchema(BlogEntrySchema);
 
 @Component({
-  selector: 'blog-overview',
-  templateUrl: './blog-overview.component.html',
-  styleUrls: ['./blog-overview.component.scss'],
+  selector: "app-blog-overview",
+  templateUrl: "./blog-overview.component.html",
+  styleUrls: ["./blog-overview.component.scss"],
   standalone: true,
-  imports: [
-    MatCardModule,
-    MatButtonModule,
-    CommonModule
-  ]
+  imports: [MatCardModule, MatButtonModule, CommonModule],
 })
 export class BlogOverviewComponent implements OnInit {
-
   blogs: BlogEntry[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.loadBlogs();
   }
 
   loadBlogs(): void {
-    this.http.get<BlogApiResponse<BlogEntry>>('https://d-cap-blog-backend---v2.whitepond-b96fee4b.westeurope.azurecontainerapps.io/entries')
-      .subscribe(data => {
+    this.http
+      .get<
+        BlogApiResponse<BlogEntry>
+      >("https://d-cap-blog-backend---v2.whitepond-b96fee4b.westeurope.azurecontainerapps.io/entries")
+      .subscribe((data) => {
         const result = ValidationSchema.safeParse(data);
 
         if (!result.success) {
--- src/app/interceptors/logging.interceptor.ts
+++ src/app/interceptors/logging.interceptor.ts
@@ -1,16 +1,24 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 @Injectable()
 export class LoggingInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Outgoing request to: ', req.url);
+  intercept(
+    req: HttpRequest<unknown>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<unknown>> {
+    console.log("Outgoing request to: ", req.url);
     return next.handle(req).pipe(
-      tap(event => {
-        console.log('Response received from: ', req.url);
-      })
+      tap((event) => {
+        console.log(event);
+      }),
     );
   }
 }
